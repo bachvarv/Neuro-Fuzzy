02215810f1ddf97aed74ab39872e047dedea4fb8
Vladislav <v.bachvarov@web.de>
2019-01-15 16:47:26 +0100

    Working Model. Doesn't seem to learn.

--- Tensorflow/ANFIS/anfis.py
+++ Tensorflow/ANFIS/anfis.py
@@ -1,37 +1,87 @@
 import tensorflow as tf
 
-
 class Anfis:
     #
-    def __init__(self, mfParameters, num_inputs):
+    def __init__(self, mfParameters, num_inputs, num_rules, num_conclusions):
         size = len(mfParameters)
+        self.mf = [None]*(size)
+        self.summ = []
+        self.x = None
+        self.normalizedMFs = [None]*(num_rules)
+        self.outputs = [tf.placeholder(dtype=tf.float32, shape=None)]*(num_conclusions)
+        self.optimizer = None
+        self.loss = None
+        self.y = tf.placeholder(name="y", shape=(1, 1), dtype=tf.float32)
+        print(self.normalizedMFs[0])
         for i in range(num_inputs):
-            self.x = tf.placeholder(name="x"+str(i), dtype=tf.float32)
+            self.x = tf.placeholder(name="x"+str(i + 1), shape=(num_inputs, 1), dtype=tf.float32)
         self.mfParameters = mfParameters
+        # print(len(self.mfParameters))
         index = 1
         for i in mfParameters:
-            with tf.variable_scope("variables", reuse=tf.AUTO_REUSE):
-                a = tf.get_variable(name="a"+str(index), dtype=tf.float32, initializer=tf.constant(i[0]))
-                m = tf.get_variable(name="m"+str(index), dtype=tf.float32, initializer=tf.constant(i[1]))
-                b = tf.get_variable(name="b"+str(index), dtype=tf.float32, initializer=tf.constant(i[2]))
+            # a = None
+            # m = None
+            # b = None
+            # with tf.variable_scope("variables", reuse=tf.AUTO_REUSE):
+            a = tf.get_variable(name="a"+str(index), dtype=tf.float32, initializer=tf.constant(i[0]), trainable=1)
+            m = tf.get_variable(name="m"+str(index), dtype=tf.float32, initializer=tf.constant(i[1]), trainable=1)
+            b = tf.get_variable(name="b"+str(index), dtype=tf.float32, initializer=tf.constant(i[2]), trainable=1)
 
-                self.triangularMF(self.x, a, m, b, index)
+            with tf.variable_scope("mfs", reuse=tf.AUTO_REUSE):
+                self.mf[(index - 1)] = self.triangularMF(self.x, a, m, b, ("mf" + str(index)))
 
             index += 1
+
+        self.normLayer()
+
+        # self.result = tf.
+
+        # output layer selbst erzeugen am Ende
+        #self.outputLayer(num_rules, num_inputs)
+
+        # self.optimizeMethod()
 
         self.trainableParams = tf.trainable_variables()
         self.init = tf.global_variables_initializer()
 
-    def triangularMF(self, x, a, m, b, index):
+    def triangularMF(self, x, a, m, b, name):
         # min_left = (x - a1) / (m1 - a1)
-        with tf.name_scope("mfs"):
-            min_left = tf.divide(tf.subtract(x, a), tf.subtract(m, a))
-            # min_right = (b1 - x) / (b1 - m1)
-            min_right = tf.divide(tf.subtract(b, x), tf.subtract(b, m))
-            min_func = tf.minimum(min_left, min_right)
-            # divisor_1 = tf.subtract(b1, a1)
-            # dividend_1 = tf.abs(tf.subtract(x, m1))
-            # m_1_func = tf.subtract(1.0, tf.multiply(2.0, tf.divide(dividend_1, divisor_1)), name="m_1")
-            m_1 = tf.maximum(min_func, 0.0, name="mf" + str(index))
+        min_left = tf.divide(tf.subtract(x, a), tf.subtract(m, a))
+        # min_right = (b1 - x) / (b1 - m1)
+        min_right = tf.divide(tf.subtract(b, x), tf.subtract(b, m))
+        min_func = tf.minimum(min_left, min_right)
+        # divisor_1 = tf.subtract(b1, a1)
+        # dividend_1 = tf.abs(tf.subtract(x, m1))
+        # m_1_func = tf.subtract(1.0, tf.multiply(2.0, tf.divide(dividend_1, divisor_1)), name="m_1")
+        m_1 = tf.maximum(min_func, 0.0, name= name)
 
         return m_1
+
+    def normLayer(self):
+        summ = 0.0
+        for i in range(len(self.mf)):
+            summ = tf.add(summ, self.mf[i])
+        for i in range(len(self.mf)):
+            self.normalizedMFs[i] = tf.divide(self.mf[i], summ)
+
+    def outputTensor(self, index, tensor):
+        self.outputs[index] = tf.multiply(self.normalizedMFs[index], tensor)
+
+    def outputLayer(self, num_rules, num_inputs):
+        # self.outputs[i] = tf.multiply(self.normalizedMFs[])
+        #TODO: connection between Produkt layer and Output layer is missing....!!!
+
+        self.summ = tf.reduce_sum(tf.reshape(self.outputs, (-1, num_rules, num_inputs)), axis=1)
+
+        self.optimizeMethod()
+
+    def doCalculation(self, sess, x):
+        return sess.run(self.summ, feed_dict={self.x: x})
+
+    def optimizeMethod(self):
+        self.loss = tf.losses.mean_squared_error(self.y, self.summ)
+
+        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(self.loss)
+
+    def train(self, sess, x, y):
+        return sess.run([self.loss, self.optimizer], feed_dict={self.x: x, self.y: y})
--- Tensorflow/tf_custom_training/test.py
+++ Tensorflow/tf_custom_training/test.py
@@ -1,25 +1,67 @@
+from random import uniform
+
 import tensorflow as tf
 
 from ANFIS.anfis import Anfis
 
 a = [[1.0, 3.0, 5.0], [5.0, 7.0, 9.0]]
 
-f = Anfis(a, 1)
+num_rules = 2
+
+num_inputs = 1
+
+num_conclusions = 2
+
+f = Anfis(a, num_inputs, num_rules, num_conclusions)
 
 for i in a:
     print(i)
 
-with tf.variable_scope("variables") as scope:
+with tf.variable_scope("") as scope:
     scope.reuse_variables()
     a1 = tf.get_variable("a1", [])
     m1 = tf.get_variable("m1", [])
     b1 = tf.get_variable("b1", [])
+
+# mfs = tf.reshape(f.normalizedMFs, shape=[])
+
+f.outputTensor(0, tf.multiply(f.x, 2))
+
+f.outputTensor(1, tf.multiply(f.x, 0.5))
+
+f.outputLayer(num_rules, num_inputs)
+
 # with tf.variable_scope("mfs") as scope:
 #     scope.reuse_variables()
 #     mf1 = tf.get_variable("mf1", [])
 
 with tf.Session() as sess:
-    # writer = tf.summary.FileWriter("output_test", graph= sess.graph)
+    writer = tf.summary.FileWriter("output_test", graph=sess.graph)
 
     sess.run(f.init)
-    # writer.close()
+    # print(sess.run(f.x[0], feed_dict={f.x[0]: 10}))
+    #
+    # print(sess.run([f.summ], feed_dict={f.x[0]: 1.3}))
+
+    # print(f.doCalculation(sess, [[7.0]]))
+
+    print(sess.run([a1, m1, b1]))
+
+    for _ in range(50):
+        # does work but doesn't seem to change the value of the variables
+        candidate = uniform(1.01, 6.32)
+        x = [[candidate]]
+        y = [[2.0 * candidate]]
+        print("input:", candidate, f.doCalculation(sess, x))
+
+        print(f.train(sess, x, y))
+
+    with tf.variable_scope("") as scope:
+        scope.reuse_variables()
+        a1 = tf.get_variable("a1")
+        m1 = tf.get_variable("m1")
+        b1 = tf.get_variable("b1")
+
+    print(sess.run([a1, m1, b1]))
+
+    writer.close()
